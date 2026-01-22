676631b0628b7b7433e675444855b0c38181a50b
lordhanya <ashifrahman8638471722@gmail.com>
2026-01-22 12:49:45 +0530

    connection.php corrected

--- db/connection.php
+++ db/connection.php
@@ -1,4 +1,12 @@
 <?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}
+
 $servername = $_ENV['DB_HOST'];
 $username = $_ENV['DB_USER_NAME'];
 $password = $_ENV['DB_PASSWORD'];
