69e216e67bc3cd7b6873d1f8704b2e55db1c1aaa
lordhanya <ashifrahman8638471722@gmail.com>
2026-01-22 12:51:07 +0530

    error handling added to connection.php

--- db/connection.php
+++ db/connection.php
@@ -14,3 +14,9 @@ $dbname = $_ENV['DB_NAME'];
 
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// if(!$conn){
+//     die("Connection Failed" . mysqli_connect_error());
+// }else{
+//     echo("Connected Successfully");
+// }
